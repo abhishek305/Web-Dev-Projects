7be36b04d4a6798978d14c9a643b12d25aa070d9
Abhishek Ezhava <abhishekshaji1994@gmail.com>
2019-07-03 22:02:10 +0530

    Add files via upload

---
+++ theme change/js/js1.js
@@ -0,0 +1,20 @@
+/*===========================================one way=========================*/
+function t1(color) {
+    document.body.style.background = color;
+ }
+/*========================using ES6===========================
+var str1 =(color)=>
+{
+    document.body.style.background=color;
+}*/
+
+/*=================change backgrd of button =======
+var str1 = (color)=>
+{
+    document.getElementById('b1').style.background=color;
+}*/
+
+
+
+
+
